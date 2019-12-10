ec6ba0a2765ffd1747c8a32d9a3c419ade4ca232
Kevin Neiger <neigerk@oregonstate.edu>
2019-12-10 09:30:34 -0800

    404.handlebars

--- server.js
+++ server.js
@@ -19,8 +19,8 @@ app.get('/', function(req, res){
 app.use(express.static('public'));
 
 app.get('*', function (req, res) {
-  res.status(404);
-  res.sendFile(__dirname + '/public/404.html');
+  res.status(404).render('404');
+  //res.sendFile(__dirname + '/public/404.html');
   console.log(req.url);
   // console.log(res);
   //res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
